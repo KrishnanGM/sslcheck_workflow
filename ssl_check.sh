99eefa4a2071433c44800e04ce01cea88cdab767
EC2 Default User <ec2-user@ip-172-31-46-103.ec2.internal>
2023-08-23 04:44:22 +0000

    Initial Commit

---
+++ ssl_check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SLACK_WEBHOOK_URL="${SLACK_WEBHOOK_URL}"
+
+check_ssl_expiry() {
+  domain="$1"
+  expiry_date=$(openssl s_client -servername $domain -connect $domain:443 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f2)
+  remaining_days=$(( ($(date -d "$expiry_date" +%s) - $(date +%s)) / 86400 ))
+
+  if [ $remaining_days -lt 10 ]; then
+    send_slack_alert "$domain" "$remaining_days"
+  fi
+}
+
+send_slack_alert() {
+  domain="$1"
+  remaining_days="$2"
+  message="SSL Expiry Alert\n   * Domain : $domain\n   * Warning : The SSL certificate for $domain will expire in $remaining_days days."
+
+  curl -X POST -H 'Content-type: application/json' --data "{
+    \"text\": \"$message\"
+  }" $SLACK_WEBHOOK_URL
+}
+
+
+IFS=$'\n' read -d '' -r -a domains < domains.txt
+
+for domain in "${domains[@]}"; do
+  check_ssl_expiry "$domain"
+done
+
